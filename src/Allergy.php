cb241fa9314a48d6ae30c17c4237e88511db8514
Alex Fallenstedt and Joe Karasek <joseph.karasek@gmail.com>
2016-02-16 14:03:20 -0800

    add returnAllergens function

--- src/Allergy.php
+++ src/Allergy.php
@@ -1,16 +1,30 @@
 <?php
     class Allergy
     {
-        
-        // function quarters($input)
+
         function scoreToBin($input)
         {
-            $input = decbin($input);//converts to binary
+            $input = decbin($input);//converts to binary<<<<<<<
             $input = strrev($input);//flips the array
-            while (strlen($input) < 8) {
+            while (strlen($input) < 8) {        
                 $input .= "0";
             }
             return $input;
+        }
+
+        function returnAllergens($string)
+        {
+            $allergens = array("eggs", "peanuts", "shellfish", "strawberries", "tomatoes", "chocolate", "pollen", "cats");//define allergens
+            $result = array();//define blank result array
+            $string = str_split($string);// converts binary string into array. $string = "10010000" --> $string = array("1", "0", "0"...)
+            foreach($allergens as $index => $allergy) {//for each allergy in $allergens, while tracking its index...
+                if ($string[$index] == '1') {//if your string array at the positiong of index...--> $string = array("1", "0", "0"...); == 1
+                    $result[$allergy] = true;//assigns into the $result array --> $result = array("eggs"=>true);
+                } else {
+                    $result[$allergy] = false;//assigns into the $result array --> $result = array("eggs"=>false);
+                }
+            }
+            return $result;
         }
 
     }
--- tests/AllergyTest.php
+++ tests/AllergyTest.php
@@ -15,5 +15,18 @@
             //assert
             $this->assertEquals("10010000", $result);
         }
+
+        function test_returnAllergens()
+        {
+            //arrange
+            $test_Allergy = new Allergy;
+            $input = "10010000";
+
+            //act
+            $result = $test_Allergy->returnAllergens($input);
+
+            //assert
+            $this->assertEquals(array("eggs"=>true, "peanuts"=>false, "shellfish"=>false, "strawberries"=>true, "tomatoes"=>false, "chocolate"=>false, "pollen"=>false, "cats"=>false), $result);
+        }
     }
 ?>
